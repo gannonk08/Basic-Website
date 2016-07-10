1a583510e2e924136aef2d0b6aa8534a33e61555
Kristjan Gannon <gannonk08@gmail.com>
2016-07-10 17:27:42 -0600

    Week One Project: Basic Website Initial Commit

---
+++ scriptname.sh
@@ -0,0 +1,3 @@
+ touch index.html && mkdir scripts && cd scripts && touch main.js && cd .. &&
+ mkdir styles && cd styles && touch main.css && cd .. && echo "<!DOCTYPE html>
+ <html><head><meta charset="utf-8"><title></title></head><body></body></html>" > index.html && cd scripts && echo "console.log('sanity check!');" > main.js &&  cd .. && cd styles && echo "h1 {font-family: Helvetica, sans serif;}" > main.css && cd ..
